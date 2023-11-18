7710ebaa2c06a04f7c5e8cc4d3542c2b70c0f226
AvdeepKaur <avdeepkaurpadda@gmail.com>
2023-11-18 15:26:08 -0500

    added collision

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -11,10 +11,11 @@ loadBean();
 
 // add character to screen, from a list of components
 const player = add([
-  sprite("bean"), // renders as a sprite
+  sprite("bean", "player"), // renders as a sprite
   pos(120, 80), // position in world
   area(), // has a collider
   body(), // responds to physics and gravity
+  "player",
 ]);
 
 // jump when player presses "space" key
@@ -31,18 +32,38 @@ add([
   area(),
   body({ isStatic: true }),
   color(127, 200, 255),
-  //projectile,
 ]);
 
 loop(3, () => {
   const projectile = add([
-    sprite("bean"),
+    sprite("bean", "projectile"),
     pos(width(), height() - 100),
     area(),
     move(900, 200),
     offscreen({ destroy: true }),
+    "projectile",
   ]);
+  projectile.onCollide("player", () => {
+    destroy(projectile);
+  });
 });
+
+const score = add([text("Score: 10"), pos(24, 24), { value: 10 }]);
+
+player.onCollide("projectile", () => {
+  score.value -= 1;
+  score.text = "Score:" + score.value;
+});
+
+// with options
+add([
+  pos(24, 24),
+  text(" ", {
+    size: 48,
+    width: 320,
+    font: "sans-serif",
+  }),
+]);
 // const follower = add([
 //   sprite("bean"),
 //   pos(110, 80), // position in world
