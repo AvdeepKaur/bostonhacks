8a8bec36a6557a6020da61ea2af3802be4440eb5
AvdeepKaur <avdeepkaurpadda@gmail.com>
2023-11-18 14:20:34 -0500

    added movement

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -4,31 +4,49 @@ import kaboom from "kaboom"
 kaboom()
 
 // define gravity
-setGravity(2400)
+setGravity(2400);
 
 // load a default sprite
-loadBean()
+loadBean();
+loadBean();
+
+add([
+  rect(width(), 48),
+  outline(4),
+  pos(0, height()),
+  anchor("botleft"),
+  area(),
+  body({ isStatic: true }),
+  color(127, 200, 255),
+]);
 
 // add character to screen, from a list of components
 const player = add([
-    sprite("bean"),  // renders as a sprite
-    pos(120, 80),    // position in world
-    area(),          // has a collider
-    body(),          // responds to physics and gravity
-])
+  sprite("bean"), // renders as a sprite
+  pos(120, 80), // position in world
+  area(), // has a collider
+  body(), // responds to physics and gravity
+]);
+
+// const follower = add([
+//   sprite("bean"),
+//   pos(110, 80), // position in world
+//   area(), // has a collider
+//   body(),
+// ]);
 
 // jump when player presses "space" key
 onKeyPress("space", () => {
-    // .jump() is provided by the body() component
-    player.jump()
-})
+  player.jump();
+  follower.jump();
+});
 
-add([
-	rect(width(), 48),
-	outline(4),
-	pos(0, height()),
-	anchor("botleft"),
-	area(),
-	body({ isStatic: true }),
-	color(127, 200, 255),
-]);
+onKeyPress("d", () => {
+  player.moveBy(10);
+  follower.moveBy(10);
+});
+
+onKeyPress("a", () => {
+  player.moveBy(-10);
+  follower.moveBy(-10);
+});
